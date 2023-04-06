1d0a81700b8aa1ed1baf41853b00e812c6073375
Sam1215Lee <120070863+Sam1215Lee@users.noreply.github.com>
2023-04-06 23:36:05 +0800

    Add files via upload

---
+++ PCA/FACE RECOGNITION/Visualize_Eigenface.m
@@ -0,0 +1,10 @@
+function Visualize_Eigenface(Eigenfaces, imgrow, imgcol)
+    Num_Eigenvalue = size(Eigenfaces,2);
+    figure('Name','Eigenface')
+    img = zeros(imgrow, imgcol);
+    for i=1:min(Num_Eigenvalue,9)
+        img(:) = Eigenfaces(:,i);
+        subplot(3,3,i);
+        imshow(img',[]);
+    end
+end
